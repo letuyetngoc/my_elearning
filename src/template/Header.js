5714f378db28507a11ccb0bb559efbc103e1da45
letuyetngoc <letuyetngocksh@gmail.com>
2022-09-24 00:56:10 +0700

    responsive homtemplate

--- src/template/Header.js
+++ src/template/Header.js
@@ -1,6 +1,7 @@
 import React, { memo, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import useAcountInfo from '../hooks/useAcountInfo'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 function Header() {
     const [isSticyMenu, setIsStickyMenu] = useState(false)
@@ -20,34 +21,39 @@ function Header() {
     return (
         <>
             <div className={`header ${isSticyMenu && 'sticky'}`}>
-                <div className='header-wrap'>
-                    <div className='header__logo'>
-                        <Link to='/home'>My Elearning</Link>
-                    </div>
-                    <ul className='header__menu'>
-                        <li className='menu-item'>
-                            <Link to='/feature' className='menu-link'>
-                                <span>Feature</span>
-                            </Link>
-                        </li>
-                        <li className='menu-item'>
-                            <Link to='/courses' className='menu-link'>
-                                <span>Courses</span>
-                            </Link>
-                        </li>
-                        <li className='menu-item'>
-                            <Link to='/dashboard' className='menu-link'>
-                                <span>Dashboard</span>
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className='header_loginBtn'>
-                        {userLogin ? <a onClick={() => navigate('/dashboardStudent/profile')} >
-                            <div className='header-user'>Hello {userLogin.hoTen} !</div>
-                        </a> :
-                            <a onClick={() => navigate('/login')}>
-                                <button className='btn btn--danger'>Sign in</button>
-                            </a>}
+                <div className='container'>
+                    <div className='header-wrap'>
+                        <div className='header__logo'>
+                            <Link to='/home'>My Elearning</Link>
+                        </div>
+                        <ul className='header__menu'>
+                            <li className='menu-item'>
+                                <Link to='/feature' className='menu-link'>
+                                    <span>Feature</span>
+                                </Link>
+                            </li>
+                            <li className='menu-item'>
+                                <Link to='/courses' className='menu-link'>
+                                    <span>Courses</span>
+                                </Link>
+                            </li>
+                            <li className='menu-item'>
+                                <Link to='/dashboard' className='menu-link'>
+                                    <span>Dashboard</span>
+                                </Link>
+                            </li>
+                        </ul>
+                        <div className='header_loginBtn'>
+                            {userLogin ? <a onClick={() => navigate('/dashboardStudent/profile')} >
+                                <div className='header-user'>Hello {userLogin.hoTen} !</div>
+                            </a> :
+                                <a onClick={() => navigate('/login')}>
+                                    <button className='btn btn--danger'>Sign in</button>
+                                </a>}
+                        </div>
+                        <div className='toggleBtn'>
+                            <AiOutlineMenu />
+                        </div>
                     </div>
                 </div>
             </div>
