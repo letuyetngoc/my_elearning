c5727fd9c3d45ce7c18aee35afb89a310423a6eb
letuyetngoc <letuyetngocksh@gmail.com>
2022-09-24 17:11:21 +0700

    responsive Header.js

--- src/template/Header.js
+++ src/template/Header.js
@@ -1,10 +1,11 @@
 import React, { memo, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import useAcountInfo from '../hooks/useAcountInfo'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 function Header() {
     const [isSticyMenu, setIsStickyMenu] = useState(false)
+    const [activeClass, setActiveClass] = useState(false)
     const navigate = useNavigate()
     const userLogin = useAcountInfo()
 
@@ -45,16 +46,42 @@ function Header() {
                         </ul>
                         <div className='header_loginBtn'>
                             {userLogin ? <a onClick={() => navigate('/dashboardStudent/profile')} >
-                                <div className='header-user'>Hello {userLogin.hoTen} !</div>
+                                <div className='header-user'>{userLogin.hoTen.slice(0, 2).toUpperCase()}</div>
                             </a> :
                                 <a onClick={() => navigate('/login')}>
                                     <button className='btn btn--danger'>Sign in</button>
                                 </a>}
                         </div>
-                        <div className='toggleBtn'>
+                        <div className='toggleBtn' onClick={() => setActiveClass(!activeClass)}>
                             <AiOutlineMenu />
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className={`overplay ${activeClass && 'active'}`} onClick={() => setActiveClass(false)}></div>
+            <div className={`header-responsive ${activeClass && 'active'}`}>
+                <div className='header-wrap'>
+                    <div className='header__logo'>
+                        <Link to='/home'>My Elearning</Link>
+                        <AiOutlineClose className='icon' onClick={() => setActiveClass(false)} />
+                    </div>
+                    <ul className='header__menu'>
+                        <li className='menu-item'>
+                            <Link to='/feature' className='menu-link'>
+                                <span>Feature</span>
+                            </Link>
+                        </li>
+                        <li className='menu-item'>
+                            <Link to='/courses' className='menu-link'>
+                                <span>Courses</span>
+                            </Link>
+                        </li>
+                        <li className='menu-item'>
+                            <Link to='/dashboard' className='menu-link'>
+                                <span>Dashboard</span>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </>
